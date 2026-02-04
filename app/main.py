f3104e31a164fbf2db89602987716ac522a682a0
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2026-02-04 13:35:33 +0000

    Demo Bandit security failure

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import subprocess   # 👈 add this line
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ def home():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+# 👇 Temporary insecure endpoint (for DevSecOps demo)
+@app.get("/test")
+def test():
+    subprocess.call("ls", shell=True)
