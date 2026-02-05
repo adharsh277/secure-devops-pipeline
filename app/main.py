91733d23b2d23c2628574d8052f48c29887fa5d2
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2026-02-05 08:28:49 +0000

    adddded a=fro the testign

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-#import subprocess   #  Remove this line wwhen its needed to showwcase it oke?
+import subprocess   #  Remove this line wwhen its needed to showwcase it oke?
 
 app = FastAPI()
 
@@ -11,7 +11,7 @@ def home():
 def health():
     return {"status": "ok"}
 
-# 👇 Temporary insecure endpoint  for demo only
-#@app.get("/test")
-#def test():
-#    subprocess.call("ls", shell=True)
+ 👇 Temporary insecure endpoint  for demo only
+@app.get("/test")
+def test():
+   subprocess.call("ls", shell=True)
